f77f82e80a185c2fdafa3ebc3246ee6f3e607d6a
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-18 19:01:49 +0100

    added attributes to weapon

--- AVD9QK_HFT_2022231.Models/Weapon.cs
+++ AVD9QK_HFT_2022231.Models/Weapon.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AVD9QK_HFT_2022231.Models
 {
+    [Table("weapons")]
     public class Weapon
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int WeaponId { get; set; }
 
         public string WeaponName { get; set; }
@@ -16,8 +22,11 @@ namespace AVD9QK_HFT_2022231.Models
 
         public string Facturer { get; set; }
 
+        [NotMapped]
+        [JsonIgnore]
         public virtual Operator Operator { get; set; }
 
+        [ForeignKey(nameof(Operator))]
         public int OperatorId { get; set; }
 
         public Weapon()
