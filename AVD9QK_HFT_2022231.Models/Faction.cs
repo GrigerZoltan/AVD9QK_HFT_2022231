5dc89eb2e892463b7a8624bb7f28be80aacfb029
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-18 18:54:20 +0100

    Added attributes to Faction

--- AVD9QK_HFT_2022231.Models/Faction.cs
+++ AVD9QK_HFT_2022231.Models/Faction.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AVD9QK_HFT_2022231.Models
 {
+    [Table("factions")]
     public class Faction
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int FactionId { get; set; }
 
         public string FactionName { get; set; }
 
         public string Nation { get; set; }
 
+        [NotMapped]
+        [JsonIgnore]
         public virtual ICollection<Operator> Operators { get; set; }
 
         public Faction()
