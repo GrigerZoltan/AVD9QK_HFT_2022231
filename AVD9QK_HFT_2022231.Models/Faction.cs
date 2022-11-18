b262ad6cc04d2e75b369d1fe77bb543ab1653e6f
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-18 14:49:46 +0100

    Faction class

---
+++ AVD9QK_HFT_2022231.Models/Faction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Models
+{
+    public class Faction
+    {
+        public int FactionId { get; set; }
+
+        public string FactionName { get; set; }
+
+        public string Nation { get; set; }
+
+        public virtual ICollection<Operator> Operators { get; set; }
+
+        public Faction()
+        {
+            this.Operators = new HashSet<Operator>();
+        }
+    }
+}
