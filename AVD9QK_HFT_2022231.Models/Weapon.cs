ee24c27fddaee8ca689564eff4e6e247de2d3215
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-18 14:54:49 +0100

    Weapon class

---
+++ AVD9QK_HFT_2022231.Models/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Models
+{
+    public class Weapon
+    {
+        public int WeaponId { get; set; }
+
+        public string WeaponName { get; set; }
+
+        public string Caliber { get; set; }
+
+        public string Facturer { get; set; }
+
+        public virtual Operator Operator { get; set; }
+
+        public int OperatorId { get; set; }
+
+        public Weapon()
+        {
+
+        }
+    }
+}
