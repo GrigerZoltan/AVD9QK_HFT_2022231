546d18e5b2078c2ec9e80974c2236719da809c7d
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-18 19:06:04 +0100

    WeaponRepository

---
+++ AVD9QK_HFT_2022231.Repository/WeaponRepository.cs
@@ -0,0 +1,36 @@
+﻿using AVD9QK_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Repository
+{
+    public class WeaponRepository : Repository<Weapon>, IRepository<Weapon>
+    {
+        public WeaponRepository(OperatorDbContext ctx) : base(ctx)
+        {
+
+        }
+
+        public override Weapon Read(int id)
+        {
+            return ctx.Weapons.FirstOrDefault(t => t.WeaponId == id);
+        }
+
+        public override void Update(Weapon item)
+        {
+            var old = Read(item.WeaponId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                if (prop.GetAccessors().FirstOrDefault(t => t.IsVirtual) == null)
+                {
+                    prop.SetValue(old, prop.GetValue(item));
+                }
+            }
+
+            ctx.SaveChanges();
+        }
+    }
+}
