cc0e6489ad061f9145bac88094fbd2fcf66318ab
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-19 13:01:59 +0100

    IOperatorLogic interface

---
+++ AVD9QK_HFT_2022231.Logic/IOperatorLogic.cs
@@ -0,0 +1,23 @@
+﻿using AVD9QK_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Logic
+{
+    public interface IOperatorLogic
+    {
+        void Create(Operator item);
+        void Delete(int id);
+        Operator Read(int id);
+        IQueryable<Operator> ReadAll();
+        void Update(Operator item);
+        IEnumerable<KeyValuePair<string, string>> FactionNamewithOperator();
+        IEnumerable<KeyValuePair<string, int>> MaxAgePerFaction();
+        IEnumerable<KeyValuePair<string, int>> MinHeightPerFaction();
+        IEnumerable<KeyValuePair<string, int>> OperatorsPerFaction();
+        IEnumerable<KeyValuePair<string, string>> OperatorsPreferredWeapon();
+    }
+}
