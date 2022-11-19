dc42d41035e4577c4cc0d73a953b7b9b335f6c11
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-19 12:51:09 +0100

    WeaponLogic class

---
+++ AVD9QK_HFT_2022231.Logic/WeaponLogic.cs
@@ -0,0 +1,45 @@
+﻿using AVD9QK_HFT_2022231.Models;
+using AVD9QK_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Logic
+{
+    public class WeaponLogic : IWeaponLogic
+    {
+        IRepository<Weapon> repo;
+
+        public WeaponLogic(IRepository<Weapon> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Weapon item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Weapon Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Weapon> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Weapon item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
