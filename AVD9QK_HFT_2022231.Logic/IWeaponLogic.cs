a9ce54121794cb4ac51b02447fbf4f80482adff9
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-19 12:48:47 +0100

    IWeaponLogic interface

---
+++ AVD9QK_HFT_2022231.Logic/IWeaponLogic.cs
@@ -0,0 +1,18 @@
+﻿using AVD9QK_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Logic
+{
+    public interface IWeaponLogic
+    {
+        void Create(Weapon item);
+        void Delete(int id);
+        Weapon Read(int id);
+        IQueryable<Weapon> ReadAll();
+        void Update(Weapon item);
+    }
+}
