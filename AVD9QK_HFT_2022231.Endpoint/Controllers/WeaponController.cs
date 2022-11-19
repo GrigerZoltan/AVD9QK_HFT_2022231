4747b6feb87dc8f081fa626ba501cf50c2851be6
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-19 15:44:32 +0100

    WeaponController

---
+++ AVD9QK_HFT_2022231.Endpoint/Controllers/WeaponController.cs
@@ -0,0 +1,52 @@
+﻿using AVD9QK_HFT_2022231.Logic;
+using AVD9QK_HFT_2022231.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeaponController : ControllerBase
+    {
+        IWeaponLogic logic;
+
+        public WeaponController(IWeaponLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Weapon> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Weapon Get(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Weapon value)
+        {
+            this.logic.Create(value);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Weapon value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
