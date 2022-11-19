d779cd2c2c70e21c107faf606fae47e8bfa3b0d8
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-19 12:47:29 +0100

    FactionLogic class

---
+++ AVD9QK_HFT_2022231.Logic/FactionLogic.cs
@@ -0,0 +1,45 @@
+﻿using AVD9QK_HFT_2022231.Models;
+using AVD9QK_HFT_2022231.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Logic
+{
+    public class FactionLogic : IFactionLogic
+    {
+        IRepository<Faction> repo;
+
+        public FactionLogic(IRepository<Faction> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Faction item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Faction Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Faction> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Faction item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
