39c34294abf00eebc2521d01999e770b17cd74c6
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-19 15:27:52 +0100

    Endpoint

--- AVD9QK_HFT_2022231.Endpoint/Startup.cs
+++ AVD9QK_HFT_2022231.Endpoint/Startup.cs
@@ -1,8 +1,14 @@
+using AVD9QK_HFT_2022231.Logic;
+using AVD9QK_HFT_2022231.Models;
+using AVD9QK_HFT_2022231.Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,28 +18,54 @@ namespace AVD9QK_HFT_2022231.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<OperatorDbContext>();
+            services.AddSingleton<IRepository<Operator>, OperatorRepository>();
+            services.AddSingleton<IRepository<Faction>, FactionRepository>();
+            services.AddSingleton<IRepository<Weapon>, WeaponRepository>();
+            services.AddSingleton<IOperatorLogic, OperatorLogic>();
+            services.AddSingleton<IFactionLogic, FactionLogic>();
+            services.AddSingleton<IWeaponLogic, WeaponLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AVD9QK_HFT_2022231.Endpoint", Version = "V1" });
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+ 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AVD9QK_HFT_2022231.Endpoint v1"));
             }
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
