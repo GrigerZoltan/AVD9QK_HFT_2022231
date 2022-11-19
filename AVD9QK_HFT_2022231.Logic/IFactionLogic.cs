cc6bbbc3a87bc7e1ff053366d30f39eff4b8d27d
ZoltanGriger <griger.zoltan98@gmail.com>
2022-11-19 12:44:47 +0100

    IFactionLogic

---
+++ AVD9QK_HFT_2022231.Logic/IFactionLogic.cs
@@ -0,0 +1,22 @@
+﻿using AVD9QK_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVD9QK_HFT_2022231.Logic
+{
+    public interface IFactionLogic
+    {
+        void Create(Faction item);
+
+        void Delete(int id);
+
+        Faction Read(int id);
+
+        IQueryable<Faction> ReadAll();
+
+        void Update(Faction item);
+    }
+}
